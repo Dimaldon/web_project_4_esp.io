b19951f3921ed3c5d221e3eb92dfce15395d2983
Julián Javier Núñez Valdovinos <julianjnv85@icloud.com>
2022-12-27 22:16:37 -0600

    galeria dinamica lista

--- app.js
+++ app.js
@@ -37,3 +37,68 @@ formElement.addEventListener("submit", handleProfileFormSubmit);
 closeButton.addEventListener("click", closeOverlay);
 editButton.addEventListener("click", openOverlay);
 document.addEventListener("keypress", keyPress);
+
+/*   ************** INCIA CODIGO DE LA GALERIA **************    */
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+
+const renderGallery = (data, parentElement) => {
+  //obtener grid de la galeria
+  const grid = document.querySelector(parentElement);
+
+  data.forEach((item) => {
+    //crea nodo contenedor
+    const element = document.createElement("div");
+    element.className = "element";
+    grid.appendChild(element);
+
+    //crea nodo del elemento de imagen
+    const image = document.createElement("img");
+    image.className = "element__image";
+    image.src = item.link;
+    element.appendChild(image);
+
+    //crea nodo del elemento title continer
+    const elementTitleContainer = document.createElement("div");
+    elementTitleContainer.className = "element__title-container";
+    element.appendChild(elementTitleContainer);
+
+    //crea nodo del elemnto title
+    const elementTitle = document.createElement("h2");
+    elementTitle.className = "element__title";
+    elementTitle.textContent = item.name;
+    elementTitleContainer.appendChild(elementTitle);
+
+    //crear nodo del elemenot button
+    const elementButtonLike = document.createElement("button");
+    elementButtonLike.className = "element__button-like";
+    elementTitleContainer.appendChild(elementButtonLike);
+  });
+};
+
+renderGallery(initialCards, ".elements__grid");
