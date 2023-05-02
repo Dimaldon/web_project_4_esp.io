cd63169c1dca33979ff42dff684e47e98a19f234
Dimaldon <julianjnv85@icloud.com>
2023-05-01 23:44:20 -0600

    hotfix: api refactorizar

--- src/components/api.js
+++ src/components/api.js
@@ -54,44 +54,20 @@ export default class Api {
   }
 
   putLikeButtonCard(cardId) {
-    return fetch(this._baseURL + "/cards/likes/" + cardId, {
-      method: "PUT",
-      headers: this._headers,
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      // si el servidor devuelve un error, rechaza el promise
-      return Promise.reject(`Error: ${res.status}`);
-    });
+    return this._handleFetch("/cards/likes/" + cardId, "PUT");
   }
 
   deleteLikeButtonCard(cardId) {
-    return fetch(this._baseURL + "/cards/likes/" + cardId, {
-      method: "DELETE",
-      headers: this._headers,
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      // si el servidor devuelve un error, rechaza el promise
-      return Promise.reject(`Error: ${res.status}`);
-    });
+    return this._handleFetch("/cards/likes/" + cardId, "DELETE");
   }
 
   updateUserMeAvatar(avatar) {
-    return fetch(this._baseURL + "/users/me/avatar", {
-      method: "PATCH",
-      headers: this._headers,
-      body: JSON.stringify({
+    return this._handleFetch(
+      "/users/me/avatar",
+      "PATCH",
+      JSON.stringify({
         avatar: avatar,
-      }),
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      // si el servidor devuelve un error, rechaza el promise
-      return Promise.reject(`Error: ${res.status}`);
-    });
+      })
+    );
   }
 }
